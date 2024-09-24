373a76f3f7e0116c0ea0fec711a05baf8840a080
Bruno Noguchi <bruno.noguchi@db1.com.br>
2024-09-24 17:30:21 -0300

    outros

--- src/main/java/com/estudos/kotlin/functions/HttpTriggerJava.kt
+++ src/main/java/com/estudos/kotlin/functions/HttpTriggerJava.kt
@@ -8,8 +8,19 @@ import com.microsoft.azure.functions.HttpStatus
 import com.microsoft.azure.functions.annotation.AuthorizationLevel
 import com.microsoft.azure.functions.annotation.FunctionName
 import com.microsoft.azure.functions.annotation.HttpTrigger
-import org.valiktor.validate
+import io.konform.validation.Validation
+import io.konform.validation.jsonschema.maxLength
+import io.konform.validation.jsonschema.maximum
+import io.konform.validation.jsonschema.minLength
+import io.konform.validation.jsonschema.minimum
 import java.util.Optional
+import kotlinx.serialization.*
+import kotlinx.serialization.json.*
+
+data class UserProfile(
+    val fullName: String,
+    val age: Int?
+)
 
 class HttpTriggerJava {
 
@@ -24,6 +35,25 @@ class HttpTriggerJava {
     ): HttpResponseMessage? {
         context.getLogger().info("Java HTTP trigger processed a request.")
 
+        var body = request.getBody().toString()
+        val userObj = Json.decodeFromString<UserProfile>(body)
+        println(userObj)
+
+        val validateUser = Validation<UserProfile> {
+            UserProfile::fullName {
+                minLength(2)
+                maxLength(100)
+            }
+
+            UserProfile::age ifPresent {
+                minimum(0)
+                maximum(150)
+            }
+        }
+
+        val invalidUser = UserProfile("A", -1)
+        val validationResult = validateUser(invalidUser)
+
         // Parse query parameter
         val query = request.getQueryParameters().get("name")
         val name = request.getBody()!!.orElse(query)
@@ -32,7 +62,7 @@ class HttpTriggerJava {
             return request.createResponseBuilder(HttpStatus.BAD_REQUEST)
                 .body("Please pass a name on the query string or in the request body").build()
         } else {
-            return request.createResponseBuilder(HttpStatus.OK).body("Hello, " + name).build()
+            return request.createResponseBuilder(HttpStatus.OK).body("Hello, $name").build()
         }
     }
 }
